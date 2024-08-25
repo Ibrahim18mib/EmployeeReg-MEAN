472abde95cebafdb00824a345f1a2fd1bcf3abca
Ibrahim18mib <ibrahimeblakwt@gmail.com>
2024-08-24 13:12:35 -1200

    added the CRUD functionalities basically and finalised.

--- Frontend/employee-reg/src/app/employeecrud/employeecrud.component.ts
+++ Frontend/employee-reg/src/app/employeecrud/employeecrud.component.ts
@@ -3,56 +3,131 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { environment } from '../../environment';
-import { constants } from 'buffer';
 import { Constants } from '../../constants/constant';
 
 @Component({
   selector: 'app-employeecrud',
   standalone: true,
-  imports: [FormsModule,CommonModule,],
+  imports: [FormsModule, CommonModule],
   templateUrl: './employeecrud.component.html',
-  styleUrl: './employeecrud.component.scss'
+  styleUrl: './employeecrud.component.scss',
 })
 export class EmployeecrudComponent implements OnInit {
+  employeeLists: any[] = [];
+  currentEmpId: any = '';
 
-  employeeLists:any[] = [];
-  currentEmpId:any = ''
+  name: string = '';
+  address: string = '';
+  phone: string = '';
 
-  name:string = '';
-  address:string = '';
-  phone:string = '';
-
-  http = inject(HttpClient)
+  http = inject(HttpClient);
 
   ngOnInit(): void {
-    this.geAllEmp()
+    this.getAllEmp();
   }
 
-  geAllEmp(){
-
-    this.http.get(environment.base_url + Constants.API_END_POINTS.GETALL)
-    .subscribe((resultData: any)=>
-    {
-       
+  getAllEmp() {
+    this.http
+      .get(environment.base_url + Constants.API_END_POINTS.GETALL)
+      .subscribe((resultData: any) => {
         console.log(resultData);
         this.employeeLists = resultData.data;
-    });
-
-
+      });
   }
 
-  save(){
-
+  register() {
+    let bodyData = {
+      name: this.name,
+      address: this.address,
+      phone: this.phone,
+    };
+    this.http
+      .post(environment.base_url + Constants.API_END_POINTS.CREATE, bodyData)
+      .subscribe((resultData: any) => {
+        try {
+          if (resultData.status) {
+            alert('Data Added Successfully');
+            this.name = '';
+            this.address = '';
+            this.phone = '';
+            this.getAllEmp();
+          } else {
+            alert('Error while Creating the data');
+          }
+        } catch (error) {
+          alert(error);
+        }
+      });
+  }
+  save() {
+    if (this.currentEmpId == '') {
+      this.register();
+    } else {
+      this.UpdateRecords();
+    }
   }
 
-  setUpdate(empData:any){
+  setUpdate(empData: any) {
+    this.name = empData.name;
+    this.address = empData.address;
+    this.phone = empData.phone;
 
+    this.currentEmpId = empData._id;
   }
 
-  setDelete(empData:any){
+  UpdateRecords() {
+    let bodyData = {
+      name: this.name,
+      address: this.address,
+      phone: this.phone,
+    };
 
+    this.http
+      .patch(
+        environment.base_url +
+          Constants.API_END_POINTS.UPDATE +
+          '/' +
+          this.currentEmpId,
+        bodyData
+      )
+      .subscribe(
+        (resultData: any) => {
+          if (resultData.status) {
+            console.log(resultData);
+            alert('Employee Updateddd');
+            this.getAllEmp();
+            this.name = '';
+            this.address = '';
+            this.phone = '';
+          } else {
+            alert('Error while updating in the Server');
+          }
+        },
+        (error) => {
+          alert(error);
+        }
+      );
   }
 
-
-
+  setDelete(empData: any) {
+    this.http
+      .delete(
+        environment.base_url +
+          Constants.API_END_POINTS.DELETE +
+          '/' +
+          empData._id
+      )
+      .subscribe((res: any) => {
+        try {
+          if (res.status) {
+            alert('Data has deleted Successfully..');
+            this.getAllEmp();
+          } else {
+            alert('Error while deleting data.');
+          }
+        } catch (error) {
+          alert(error);
+        }
+      });
+  }
 }
--- Frontend/employee-reg/src/constants/constant.ts
+++ Frontend/employee-reg/src/constants/constant.ts
@@ -2,7 +2,7 @@ export const Constants = {
 API_END_POINTS: {
     GETALL:'/user/getall',
     CREATE:'/user/create',
-    UPDATE:'/user/update/:id',
-    DELETE:'/user/delete/:id'
+    UPDATE:'/user/update',
+    DELETE:'/user/delete'
 }
 }
